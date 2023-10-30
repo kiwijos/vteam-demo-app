b97a13c001c96bd130801974b098f7a75f7d6ca3
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-10-30 09:51:29 +0100

    Create navigation component

---
+++ src/lib/components/Navigation.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { getDrawerStore } from '@skeletonlabs/skeleton';
+
+	const drawerStore = getDrawerStore();
+
+	function drawerClose(): void {
+		drawerStore.close();
+	}
+</script>
+
+<nav class="list-nav p-4">
+	<ul>
+		<li><a href="/" on:click={drawerClose}>Home</a></li>
+		<li><a href="/about" on:click={drawerClose}>About</a></li>
+		<li><a href="/map" on:click={drawerClose}>Map</a></li>
+	</ul>
+</nav>
