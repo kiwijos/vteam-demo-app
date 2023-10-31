2a8fdecff668d021141edb5025cfe31e4803b09c
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-10-31 09:17:47 +0100

    Wrap content in card

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,12 +1,14 @@
 <div class="mx-auto p-8 ">
-    <div class="p-4 space-y-4">
-        <h2 class="h2">About Us</h2>
-        <p>
-            We are a dedicated team of web developers united by a common goal - to transform urban mobility for a cleaner, more efficient cityscape. In the spirit of collaboration, our project brings together the skills and passion of our team members to make a lasting impact.
-            As a virtual team, we've embraced the opportunities and challenges of remote collaboration, honing our skills not only in web development but also in effective teamwork. We're excited to present our project and share our vision for the future of urban mobility with you.
-        </p>
-        <p>
-            Thank you for joining us on this journey towards a more sustainable and efficient city.
-        </p>
+    <div class="card variant-glass-surface p-4">
+        <div class="p-4 space-y-4">
+            <h2 class="h2">About Us</h2>
+            <p>
+                We are a dedicated team of web developers united by a common goal - to transform urban mobility for a cleaner, more efficient cityscape. In the spirit of collaboration, our project brings together the skills and passion of our team members to make a lasting impact.
+                As a virtual team, we've embraced the opportunities and challenges of remote collaboration, honing our skills not only in web development but also in effective teamwork. We're excited to present our project and share our vision for the future of urban mobility with you.
+            </p>
+            <p>
+                Thank you for joining us on this journey towards a more sustainable and efficient city.
+            </p>
+        </div>
     </div>
 </div>
