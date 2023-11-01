229821fcd84763537e9889d00b4960463d776926
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-11-01 19:20:20 +0100

    Add points to display station names

--- src/routes/map/+page.svelte
+++ src/routes/map/+page.svelte
@@ -4,6 +4,7 @@
 	import { map } from '$lib/stores/map';
 	import type { PageData } from './$types';
 	import type { ChargingStation } from '$lib/types/ChargingStation';
+	import polylabel from 'polylabel';
 
 	export let data: PageData;
 
@@ -38,6 +39,37 @@
 				'paint': {
 					'fill-color': '#088',
 					'fill-opacity': 0.8
+				}
+			});
+
+			const pointFeatures = data.data.map((station: ChargingStation) => {
+				return {
+					type: "Feature",
+					geometry: {
+						type: "Point",
+						coordinates: polylabel(station.geometry.coordinates, 1.0)
+					},
+					properties: { name: station.name }
+				}
+			});
+
+			_map.addSource("stationNames", {
+				type: "geojson",
+				data: {
+					type: "FeatureCollection",
+					features: pointFeatures
+				}
+			});
+			_map.addLayer({
+				id: "stationNames",
+				type: "symbol",
+				source: "stationNames",
+				layout: {
+					// get the name from the source's "name" property
+					"text-field": ["get", "name"],
+					"text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+					"text-offset": [0, 1.25],
+					"text-anchor": "top"
 				}
 			});
 	})};
