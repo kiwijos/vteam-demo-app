2bcec0f7f77e475976e96fa14657c231b034189c
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-11-01 19:16:55 +0100

    Create API endpoints to serve sample data

---
+++ src/routes/api/charging/+server.ts
@@ -0,0 +1,35 @@
+import { json } from '@sveltejs/kit';
+import type { ChargingStation } from '$lib/types/ChargingStation';
+import { chargingStations } from '$lib/data/chargingStations';
+
+async function getStations({
+	start = 0,
+	end = 10,
+	stationId = null
+}: {
+	start: number;
+	end: number;
+	stationId: number;
+}) {
+	// simulate delay when retriving the chargingStations with a promise that resolves after 500ms with the chargingStations
+	const stationsData = await new Promise<ChargingStation[]>((resolve) =>
+		setTimeout(() => resolve(chargingStations), 500)
+	);
+
+	if (stationId) {
+		const stationsWithId = stationsData.filter((station) => station.id === stationId);
+		return stationsWithId.slice(start, end);
+	} else {
+		return stationsData.slice(start, end);
+	}
+}
+
+export async function GET({ url }) {
+	const start = Number(url.searchParams.get('start')) || 0;
+	const end = Number(url.searchParams.get('end')) || 10;
+	const stationId = Number(url.searchParams.get('stationId')) || null;
+
+	const stationsData = await getStations({ start, end, stationId });
+
+	return json(stationsData);
+}
---
+++ src/routes/api/parking/+server.ts
@@ -0,0 +1,35 @@
+import { json } from '@sveltejs/kit';
+import type { ParkingStation } from '$lib/types/ParkingStation';
+import { parkingStations } from '$lib/data/parkingStations';
+
+async function getParking({
+	start = 0,
+	end = 10,
+	parkingId = null
+}: {
+	start: number;
+	end: number;
+	parkingId: number;
+}) {
+	// simulate delay when retriving the parkingStations with a promise that resolves after 500ms with the parkingStations
+	const stationsData = await new Promise<ParkingStation[]>((resolve) =>
+		setTimeout(() => resolve(parkingStations), 500)
+	);
+
+	if (parkingId) {
+		const stationsWithId = stationsData.filter((station) => station.id === parkingId);
+		return stationsWithId.slice(start, end);
+	} else {
+		return stationsData.slice(start, end);
+	}
+}
+
+export async function GET({ url }) {
+	const start = Number(url.searchParams.get('start')) || 0;
+	const end = Number(url.searchParams.get('end')) || 10;
+	const parkingId = Number(url.searchParams.get('parkingId')) || null;
+
+	const stationsData = await getParking({ start, end, parkingId });
+
+	return json(stationsData);
+}
