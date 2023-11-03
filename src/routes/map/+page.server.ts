5bbdf9d07bc3d4ec64f94fc8baa82d05c15de8db
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-11-03 14:51:26 +0100

    Call /parking/within endpoint to retrive stations based on city bounds

--- src/routes/map/+page.server.ts
+++ src/routes/map/+page.server.ts
@@ -1,7 +1,7 @@
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ fetch }) => {
-	const res = await fetch('/api/parking');
+	const res = await fetch('/api/parking/within?cityName=Oskarshamn');
 	const data = await res.json();
 
 	return { data };
