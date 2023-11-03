1733d02021774d61dc7b9083fd9a874ee6d45af2
Alex kiwijos Lindqvist <allq22@student.bth.se>
2023-11-03 23:25:23 +0100

    Extract and type data property

--- src/routes/map/+page.server.ts
+++ src/routes/map/+page.server.ts
@@ -1,8 +1,11 @@
 import type { PageServerLoad } from './$types';
+import type { ParkingStation } from '$lib/types/ParkingStation';
 
 export const load: PageServerLoad = async ({ fetch }) => {
 	const res = await fetch('/api/parking/within?cityName=Oskarshamn');
-	const data = await res.json();
+	const result = await res.json();
+
+	const data: ParkingStation[] = result.data;
 
 	return { data };
 };
